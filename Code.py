6a8d195ae81b69f0a983058f6e4ae4d6874fc34c
Venkata Sai Naveena Bathula <vbathula@hawk.iit.edu>
2021-08-18 01:00:54 -0500

    First commit

--- Code.py
+++ Code.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 from datetime import datetime
 
 FILE_NAME = "api.csv"
@@ -7,23 +6,16 @@ def convert_log_to_csv(path):
     file = pd.read_csv(path, sep=' ', header=None)
     file = file.dropna(axis=1)
     file.columns = ['Date', 'Time', 'Logger', 'Alert', 'Code', 'Log_Message']
-    print(file)
     file['Combineddatetime'] = file['Date']+ ' '+ file['Time']
-    print(file)
     file= file.groupby(['Code']).agg(
         {'Log_Message': lambda x: '-'.join(x), 'Combineddatetime': lambda x: '/'.join(x)})
     file[['Start_Time', 'End_Time']] = file['Combineddatetime'].str.split('/', 1, expand=True)
-
     file['S'] = pd.to_datetime(file['Start_Time'])
-    print(type(file['S'].tolist()[2]))
     file['E'] = pd.to_datetime(file['End_Time'])
     file['Time_diff']= file['E']-file['S']
     file['Time_diff']= file.Time_diff.dt.total_seconds()
-    print(file.columns)
-
     file.drop(['Combineddatetime','S','E'],axis=1,inplace=True)
     file.to_csv(FILE_NAME,index = False)
-    print(file.dtypes)
 
 if __name__ == "__main__":
     convert_log_to_csv("api.log")
